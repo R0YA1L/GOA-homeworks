e1dd85f3f6d28812b6b8e0b35b17d60b8ff7a68c
ROYA1L <nikaxuco24@gmail.com>
2024-02-13 20:16:12 +0400

    first commit

---
+++ day 2/lesson1 .py
@@ -0,0 +1,18 @@
+name = "nika"
+
+
+surname = "khutsishvili"
+
+print(name)
+print(surname)
+
+age = 12
+heigt = 145.7
+
+knows_programming = True
+
+print( name + " " + surname)
+
+print(age + str(age))
+
+
---
+++ day 3/classwork/lesson3 .py
@@ -0,0 +1,17 @@
+name = "nika"
+surname = "khutsihvili"
+
+print(name + " " + surname)
+
+
+country = "spain,"
+capital = "madrid."
+
+print(country + "" + capital)
+
+is_pretty = "(false)"
+
+question = "is pretty"
+
+print(question)
+print(is_pretty)
