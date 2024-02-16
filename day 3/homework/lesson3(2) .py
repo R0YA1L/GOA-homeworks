c0361fd578c10fc50ed5ea1c465d103b756114aa
ROYA1L <nikaxuco24@gmail.com>
2024-02-16 17:52:26 +0400

    second commit

---
+++ day 3/homework/lesson3(2) .py
@@ -0,0 +1,38 @@
+value = "dad ="
+name = " daviti"
+
+print(value + name)
+
+value = "mom ="
+name = " ledi" #she has unique name ;)
+
+print(value + name)
+
+value = "sister ="
+name = " salome"
+
+print(value + name)
+
+value = "grandpa ="
+name = " nikolozi"
+
+print(value + name)
+
+value = "grandma ="
+name = " dali"
+
+print(value + name)
+
+value = "uncle ="
+name = " iosebi"
+
+print(value + name)
+#uncle 2
+
+value = "uncle ="
+name = " giorgi"
+
+print(value + name)
+
+#homework done :D
+#question do they see what i write here. like the academy do they see my comments and stuff when i upload to github? anyways bye
