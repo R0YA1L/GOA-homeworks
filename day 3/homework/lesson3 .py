1ee91d4e5af2304cb0816e5a220fc70b1f5a198a
ROYA1L <nikaxuco24@gmail.com>
2024-02-17 17:12:36 +0400

    third commit

---
+++ day 3/homework/lesson3 .py
@@ -0,0 +1,4 @@
+#making the guy mad (need 20 errors)
+
+
+prin(s) y o u a r e s m a r t a n d p r e t t y
---
+++ day 4/homework4 .py
@@ -0,0 +1,15 @@
+#making my information in terminal by using verbial and values
+
+name = "nika"
+surname = "khutsishvili"
+age = "12"
+age_after_10_years = "22"
+country = "georgia"
+city = "varketili"
+
+print("my name is" " " + name)
+print("my surname is" " " + surname)
+print("my age is" ' ' + age)
+print("my age after 10 years will be" " " + age_after_10_years)
+print("my native country is" " " + country)
+print("i live in" " " + city)
