5d74ca99002864e8f4926b25b1a7a5055e05ac66
ROYA1L <nikaxuco24@gmail.com>
2024-02-22 18:20:16 +0400

    fourth commit

---
+++ day 5/homework5 .py
@@ -0,0 +1,52 @@
+#printing my family age and age after 5 years by using verbials and values
+
+# dad = 39
+
+# print(dad)
+
+# dad_5 = 44
+
+# print(dad_5)
+
+# mom = 39
+
+# print(mom)
+
+# mom_5 = 44
+
+# print(mom_5)
+
+mom = 39
+dad = 39
+sis = 10
+grandpa = 69
+grandma = 63
+uncle = 30
+me = 12
+
+print("my mom is "  + str(mom) + " years old")
+print("my dad is " + str(dad) + " years old")
+print("my sister is " + str(sis) + " years old")
+print("my grandpa is " + str(grandpa) + " years old")
+print("my grandma is " + str(grandma) + " years old")
+print("my uncle is " + str(uncle)  +  " years old")
+print("i am " + str(me) + " years old")
+#now i will make it how old they will be after 5 years
+
+mom_5 = 44
+dad_5 = 44
+sis_5 = 15
+grandpa_5 = 74
+grandma_5 = 68
+uncle_5 = 35    
+me_5 = 17
+
+print(" my family after 5 years will be:")
+
+print("my mom is "  + str(mom_5) + " years old")
+print("my dad is " + str(dad_5) + " years old")
+print("my sister is " + str(sis_5) + " years old")
+print("my grandpa is " + str(grandpa_5) + " years old")
+print("my grandma is " + str(grandma_5) + " years old")
+print("my uncle is " + str(uncle_5)  +  " years old")
+print("i am " + str(me_5) + " years old")
