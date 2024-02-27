f9e46085d31c223016e218ea11f809d1f2767a9c
ROYA1L <nikaxuco24@gmail.com>
2024-02-27 11:52:22 +0400

    sixth commit

---
+++ day 7/homework 7 .py
@@ -0,0 +1,21 @@
+#homework
+
+age1 = int(input(' enter your age: '))
+age2 = int(input(' enter your age: '))
+age3 = int(input(' enter your age: '))
+age4 = int(input(' enter your age: '))
+age5 = int(input(' enter your age: '))
+
+print(age1*age2//age3*age4+age5)
+
+
+num = int(input())
+minus = int(input())
+num2 = int(input())
+answear = num + minus + num2
+print(answear)
+
+
+
+
+
