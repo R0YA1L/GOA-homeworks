a4d9c4187ce1bd2f192c43b1f247cdabbeda73a4
ROYA1L <nikaxuco24@gmail.com>
2024-03-23 00:41:31 +0400

    nineth commit

---
+++ day 12/homework 12 (1) .py
@@ -0,0 +1,3 @@
+number = input('Enter number: ')
+for i in range(0, 100,):
+    print(number)
---
+++ day 12/homework 12 .py
@@ -0,0 +1,9 @@
+password = 'nika'
+confrim = input('confrim your password: ')
+while confrim != password:
+    confrim = input() if confrim != password:
+    print('incorrect')
+print('correct')
+
+
+     
