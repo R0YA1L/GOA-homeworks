19ba86ed734f6d4fec8335c65811809fa2cada66
ROYA1L <nikaxuco24@gmail.com>
2024-04-14 18:38:19 +0400

    commit

---
+++ day 16/homework 16 (1) .py
@@ -0,0 +1,9 @@
+point = int(input('Enter your point: '))
+if point == 10:
+    print(' your son is exelent miss karen')
+if point == 9 or point == 8:
+    print('keep eye on your son miss karen!')
+if point == 7 or point == 6 or point == 5:
+    print('miss karen your son is not  studying!!')
+elif point < 5:
+    print('you are expelled from school johnny!!!')
