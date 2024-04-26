d11f9c1819de8c770b84193e9fd7f633304fbaac
ROYA1L <nikaxuco24@gmail.com>
2024-04-26 21:08:46 +0400

    commit

---
+++ day 19/homework 19 .py
@@ -0,0 +1,8 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+print(list[0:3])
+print(list[6:11])
+print(list[3:6])
+
+word = 'how are you today'
+print(word[3:])
+print(word[12:])
---
+++ day 20/homework/tast 2 .py
@@ -0,0 +1,8 @@
+'''2)  შექმენით ცარიელი სია. მომხმარებელს შეეკითხეთ სახელი და გვარი, რომელსაც დაამატებთ თქვენ მიერ შექმნილ სიაში
+. საბოლო შედეგი გამოიტანეთ ტერმინალში'''
+list = []
+name = input('Enter your name: ')
+last_name = input('Enter your last name: ')
+list.append(name)
+list.append(last_name)
+print(list)
